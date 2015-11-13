cfeb470701257ae5ebe7f20f2d9eb292688947b5
thehme <bcortez23@gmail.com>
2015-11-13 16:27:32 -0500

    added view calls

--- public/assets/js/views/app.view.js
+++ public/assets/js/views/app.view.js
@@ -6,9 +6,9 @@ define([
     'backbone',
     'collections/insurance.data.collection',
     'models/insurance.data.model',
-    'views/insurance.view'
+    'views/insurance.graph.view'
 ], function(
-        $,_,Backbone,InsuranceDataCollection,InsuranceDataModel,InsuranceView
+        $,_,Backbone,InsuranceDataCollection,InsuranceDataModel,InsuranceGraphView
     ){
     var AppView = Backbone.View.extend({
         el: $('#myApp'),
@@ -19,15 +19,21 @@ define([
         },
         procData:function(){
             console.log(this);
+            //calculate insurance/uninsured values
             this.insuranceData.each(function(model){
                 model.set({'percentUninsured': (model.attributes.number_uninsured/model.attributes.population)*100});
                 model.set({'percentInsured': (model.attributes.number_insured/model.attributes.population)*100});
             },this);
+            //call views
+            var lineGraphView = new InsuranceGraphView();
+            this.$('.main').append(lineGraphView.render().el);
+            var columnGraphView = new InsuranceGraphView();
+            this.$('.main').append(columnGraphView.render().el);
         },
         render: function () {
             //var insuranceView = new InsuranceView();
             //this.$('main').append(insuranceView.render().el);
-            //return this;
+            return this;
         }
     });
     return AppView;
